ac2b509d925063d7a2d0a4a1404f1c75cfda9d35
bWyatt2026 <blake.wyatt@stu.daviess.kyschools.us>
2025-04-17 02:49:09 +0000

    s

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,6 +4,27 @@ public class BinarySearch
 {
 	public static int binarySearch(int[] ray, int item)
 	{
+		Arrays.sort(ray);
+	    int bottom = 0;
+		int top = ray.length-1;
+		int middle = 0;
 
+		while(bottom <= top)
+		{
+			middle = (bottom+top)/2;
+			if(ray[middle] < item)
+			{
+				bottom = middle+1;
+			}
+			if(ray[middle] > item)
+			{
+				top = middle -1;
+			}
+			if(ray[middle] == item)
+			{
+				return middle;
+			}
+		}
+		return -1;
 	}
 }
